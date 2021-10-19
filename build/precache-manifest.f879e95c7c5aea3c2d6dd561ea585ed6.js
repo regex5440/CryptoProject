babbcedbf3ae9a6fb9499a6e720947902d70b436
Harsh Dagar <34965581+regex5440@users.noreply.github.com>
2021-10-19 14:45:46 +0530

    build

---
+++ build/precache-manifest.f879e95c7c5aea3c2d6dd561ea585ed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21045b4996f1cfbebc3eefcf0da2289",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9981c81d76c513175919",
+    "url": "/static/css/main.fcf1bc2e.chunk.css"
+  },
+  {
+    "revision": "762527f36485c9b5db84",
+    "url": "/static/js/2.26f8b729.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.26f8b729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1b754f211c398fe64b",
+    "url": "/static/js/3.e7b9f486.chunk.js"
+  },
+  {
+    "revision": "9981c81d76c513175919",
+    "url": "/static/js/main.557bfe83.chunk.js"
+  },
+  {
+    "revision": "6e0a6ac1590511edaa77",
+    "url": "/static/js/runtime-main.afcf0715.js"
+  }
+]);
