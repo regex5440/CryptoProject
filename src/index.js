9040fba0b90fd61cec439c5637f3171bae434872
Harsh Dagar <34965581+regex5440@users.noreply.github.com>
2021-10-19 16:13:43 +0530

    Remove header for netlify

--- src/index.js
+++ src/index.js
@@ -146,8 +146,7 @@ class CoinsLive extends React.Component {
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Content-Type': 'application/json'
       },
       redirect: 'follow', // manual, *follow, error
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
